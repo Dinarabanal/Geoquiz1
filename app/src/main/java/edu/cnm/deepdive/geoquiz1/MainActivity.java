6feb4693077ed80da676749260097c9162540b4c
Dina <dmrabanal@gmail.com>
2018-10-15 22:06:36 -0600

    chapter 2

--- app/src/main/java/edu/cnm/deepdive/geoquiz1/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/geoquiz1/MainActivity.java
@@ -5,36 +5,78 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
-  private Button mTrueButton;
-  private Button mFalseButton;
+  private Button TrueButton;
+  private Button FalseButton;
+  private Button NextButton;
+  private TextView QuestionTextView;
+
+  private Question[] QuestionBank = new Question[] {
+      new Question(R.string.question_australia, true),
+      new Question(R.string.question_oceans, true),
+      new Question(R.string.question_mideast, false),
+      new Question(R.string.question_africa, false),
+      new Question(R.string.question_asia, true),
+      new Question(R.string.question_americas, true),
+  }
+;
+  private int CurrentIndex = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    QuestionTextView = (TextView) findViewById(R.id.true_button);
 
-    mTrueButton = (Button) findViewById(R.id.false_button);
-    mTrueButton.setOnClickListener(new View.OnClickListener() {
+    TrueButton = (Button) findViewById(R.id.false_button);
+    TrueButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
-
+        checkAnswer(true);
       }
 
     });
 
-    mFalseButton = (Button) findViewById(R.id.false_button);
-    mFalseButton.setOnClickListener(new View.OnClickListener() {
+    FalseButton = (Button) findViewById(R.id.false_button);
+    FalseButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+       checkAnswer(false);
       }
 
-
     });
+    NextButton = (Button) findViewById(R.id.next_button);
+    NextButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        CurrentIndex = (CurrentIndex + 1) % QuestionBank.length;
+
+      }
+    });
+    private void updateQuestion () {
+      int question = QuestionBank[CurrentIndex].getTextResId();
+      QuestionTextView.setText();
+    }
   }
+  private void checkAnswer(boolean userPressTrue) {
+    boolean answerIsTrue = QuestionBank[CurrentIndex].isAnswerTrue();
+
+    int messageResId = 0
+
+        if (userPressTrue == answerIsTrue) {
+          messageResId = R.string.correct_toast;
+
+        } else {
+          messageResId = R.string.incorrect_toast;
+
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show();
+  }
+
 }
---
+++ app/src/main/java/edu/cnm/deepdive/geoquiz1/Question.java
@@ -0,0 +1,27 @@
+package edu.cnm.deepdive.geoquiz1;
+
+public class Question {
+  private int TextResId;
+  private boolean AnswerTrue;
+
+  public int getTextResId() {
+    return TextResId;
+  }
+
+  public void setTextResId(int TextResId) {
+   TextResId = TextResId;
+  }
+
+  public boolean isAnswerTrue() {
+    return AnswerTrue;
+  }
+
+  public void setAnswerTrue(boolean AnswerTrue) {
+    AnswerTrue = answerTrue;
+  }
+
+  public Question(int textResId, boolean AnswerTrue) {
+
+  }
+
+}
